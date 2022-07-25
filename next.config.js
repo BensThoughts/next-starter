65f3ecda1abb884c0cedeb26572e76474c46ba42
Benjamin <BensThoughts@Gmail.Com>
2022-07-25 07:55:20 -0700

    added bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
+  // target: 'serverless',
+  swcMinify: true,
+  reactStrictMode: true,
+  // productionBrowserSourceMaps: true,
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  // webpack: (config, {isServer}) => {
+  //   if (!isServer) {
+  //     config.node = {
+  //       fs: 'empty',
+  //     };
+  //   }
+  //   return config;
+  // },
+});
+
+module.exports = nextConfig;
